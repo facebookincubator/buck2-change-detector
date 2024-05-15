4121613e3f4a1e6604b6a79f9ceacdf866be0a3d
Neil Mitchell <ndmitchell@meta.com>
2024-05-15 08:22:39 -0700

    Refactor the testing of globs
    Summary: Make it easier to test multiple globs at once.
    
    Reviewed By: aniketmathur
    
    Differential Revision: D57344553
    
    fbshipit-source-id: 98ab3c7a7e4cef539e635e516978463c6e12dc97

--- btd/src/buck/glob.rs
+++ btd/src/buck/glob.rs
@@ -41,26 +41,31 @@ impl GlobSpec {
 mod tests {
     use super::*;
 
+    fn many(globs: &[&str], path: &str, res: bool) {
+        assert_eq!(
+            res,
+            GlobSpec::new(&globs.iter().map(|x| Glob::new(x)).collect::<Vec<_>>())
+                .matches(&ProjectRelativePath::new(path)),
+            "With {globs:?} and {path:?}"
+        )
+    }
+
+    fn one(glob: &str, path: &str, res: bool) {
+        many(&[glob], path, res);
+    }
+
     #[test]
     fn test_glob() {
-        fn f(glob: &str, path: &str, res: bool) {
-            assert_eq!(
-                res,
-                GlobSpec::new(&[Glob::new(glob)]).matches(&ProjectRelativePath::new(path)),
-                "With {glob:?} and {path:?}"
-            )
-        }
-
-        f("abc*", "abcxyz", true);
-        f("abc*", "abcxyz/bar", false);
-        f("foo/*", "foo/abc", true);
-        f("foo/*", "foo/abc/bar", false);
-        f("**/*.java", "foo/bar/baz/me.java", true);
-        f("**/*.java", "foo/bar/baz/me.jar", false);
-        f("simple", "simple", true);
-        f("foo/bar/**", "foo/bar/baz/qux.txt", true);
-        f("foo/bar/**", "foo/bar/magic", true);
-        f("foo/bar/**", "foo/bard", false);
-        f("foo/bar/**", "elsewhere", false);
+        one("abc*", "abcxyz", true);
+        one("abc*", "abcxyz/bar", false);
+        one("foo/*", "foo/abc", true);
+        one("foo/*", "foo/abc/bar", false);
+        one("**/*.java", "foo/bar/baz/me.java", true);
+        one("**/*.java", "foo/bar/baz/me.jar", false);
+        one("simple", "simple", true);
+        one("foo/bar/**", "foo/bar/baz/qux.txt", true);
+        one("foo/bar/**", "foo/bar/magic", true);
+        one("foo/bar/**", "foo/bard", false);
+        one("foo/bar/**", "elsewhere", false);
     }
 }
