7914c4bc38b77d4f1f6129c1b848629d5c328bc3
Ri Xu <rixu@meta.com>
2024-06-27 10:13:16 -0700

    cleanup cruise control knob
    Summary:
    - cleanup cruise control knob as it's fully rolled out
    - add some comments
    
    Reviewed By: Acesine
    
    Differential Revision: D59076184
    
    fbshipit-source-id: c20f951b26b48205176003a92d81100bb2a26b79

--- td_util/src/supertd_events.rs
+++ td_util/src/supertd_events.rs
@@ -35,8 +35,6 @@ pub enum Event {
     VERIFIABLE_MATCHER_SUCCESS,
     VERSE_SUCCESS,
     BUILD_DIRECTIVES_SPECIFIED,
-    VERSE_SANDCASTLE_CRUISECONTROL,
-    VERSE_MLX_CRUISEONTROL,
 }
 
 /// Initialize the Scuba client for the `supertd_events` dataset.
