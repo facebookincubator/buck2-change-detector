efeace5a099fe5b6943075cf4819e5ae0dc19111
David Tolnay <dtolnay@meta.com>
2024-08-09 15:47:38 -0700

    Update fbcode and xplat Rust toolchain
    Summary:
    Release notes:
    
    - 1.80.0: https://blog.rust-lang.org/2024/07/25/Rust-1.80.0.html
    - 1.80.1: https://blog.rust-lang.org/2024/08/08/Rust-1.80.1.html
    
    Changes affecting fbsource:
    
    - New algorithm for determining what is unused code
        - ~~Large number of bogus `dead_code` warnings~~ (fixed in 1.80.1)
        - Small number of valid new `dead_code` warnings (D60317005)
    - Changes to how `cargo vendor` works on git dependencies, requiring change to Reindeer and new fixups (D60436187, D60855541)
    - Different signature for unstable `core::ptr::from_raw_parts`, requiring Bindgen change (D60436185)
    - New clippy lints (D60469686, D60933067)
        - `clippy::doc_lazy_continuation`
        - `clippy::mutable_key_type`
        - `clippy::needless_borrows_for_generic_args`
    - Identifiers used in `cfg` attribute are now checked against a table of valid cfg expressions given by Cargo.toml, which requires Autocargo support (D60706830, D60706859, D60933080)
    - Feature stabilizations
        - `feature(byte_slice_trim_ascii)`
        - `feature(exclusive_range_pattern)`
        - `feature(ip_bits)`
        - `feature(lazy_cell)` (widely used in fbcode)
    
    Reviewed By: diliop, shayne-fletcher
    
    Differential Revision: D60317004
    
    fbshipit-source-id: 06ba46e233bd56d8420f0de7f2d99c4c0953f59c

--- btd/src/lib.rs
+++ btd/src/lib.rs
@@ -11,7 +11,6 @@
 // and we'd like to use doctests.
 
 #![feature(exit_status_error)]
-#![feature(lazy_cell)]
 #![forbid(unsafe_code)]
 // Things we disagree with
 #![allow(clippy::len_without_is_empty)]
