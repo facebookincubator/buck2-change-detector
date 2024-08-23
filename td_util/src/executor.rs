8cc88c3e6d78482e333e298c7065b3d62bbfa385
Fedir Panasenko <pfa@meta.com>
2024-08-23 09:35:50 -0700

    Make run_as_sync safe to use if runtime is present
    Summary:
    The issue with the way we use block_on() is that it tries to unconditionally create new Tokio runtime and panics if there is one already (because nested runtimes are not allowed).
    
    Here if the runtime is present, we reuse it instead of creating a new one.
    
    Reviewed By: ndmitchell
    
    Differential Revision: D61635145
    
    fbshipit-source-id: caed839c5e42ddf222d6770b1d3949228beb32c5

--- td_util/src/executor.rs
+++ td_util/src/executor.rs
@@ -11,10 +11,35 @@
 
 use std::future::Future;
 
+// Run future within a sync context
+// Uses existing Tokio runtime if available, otherwise creates a new one.
 pub fn run_as_sync<F: Future>(future: F) -> F::Output {
-    tokio::runtime::Builder::new_current_thread()
-        .enable_all()
-        .build()
-        .unwrap()
-        .block_on(future)
+    match tokio::runtime::Handle::try_current() {
+        Ok(handle) => {
+            let _guard = handle.enter();
+            futures::executor::block_on(future)
+        }
+        Err(_) => tokio::runtime::Builder::new_current_thread()
+            .enable_all()
+            .build()
+            .unwrap()
+            .block_on(future),
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::executor::run_as_sync;
+
+    #[test]
+    fn test_run_as_sync_without_runtime() {
+        // Should not panic and should return the result of the future.
+        assert_eq!(run_as_sync(async { 42 }), 42);
+    }
+
+    #[fbinit::test]
+    async fn test_run_as_sync_with_runtime() {
+        // Tokio runtime is injected by fbinit. Still, should not panic and should return the result of the future.
+        assert_eq!(run_as_sync(async { 42 }), 42);
+    }
 }
