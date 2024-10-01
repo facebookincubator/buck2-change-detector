905ca57f91442c4e4ab71c5a32b1ad5413bf250a
Prateek Goyal <prgo@meta.com>
2024-10-01 11:25:19 -0700

    Use consistent pass rate for rl_translator_continuous_diff_roll_out
    Summary: To limit blast radium and enable same job passing the jk roll out
    
    Reviewed By: 8Keep
    
    Differential Revision: D63660664
    
    fbshipit-source-id: f08e0fe963aae1a51af507075ee3a6a39a1271e9

--- td_util/src/knobs.rs
+++ td_util/src/knobs.rs
@@ -28,6 +28,26 @@ pub fn check_boolean_knob_with_switch(name: &str, switch_val: Option<&str>, defa
 }
 
 #[cfg(any(not(fbcode_build), not(target_os = "linux")))]
+pub fn check_boolean_knob_with_switch_and_consistent_pass_rate(
+    name: &str,
+    hash_val: Option<&str>,
+    switch_val: Option<&str>,
+    default: bool,
+) -> bool {
+    default
+}
+
+#[cfg(all(fbcode_build, target_os = "linux"))]
+pub fn check_boolean_knob_with_switch_and_consistent_pass_rate(
+    name: &str,
+    hash_val: Option<&str>,
+    switch_val: Option<&str>,
+    default: bool,
+) -> bool {
+    justknobs::eval(name, hash_val, switch_val).unwrap_or(default)
+}
+
+#[cfg(any(not(fbcode_build), not(target_os = "linux")))]
 pub fn check_integer_knob(_name: &str, default_value: i64) -> i64 {
     default_value
 }
