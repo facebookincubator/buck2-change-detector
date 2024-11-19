c0e3693c7d13fe25b6639411f9cc129de6e6e2e1
Ron Mordechai <ronmrdechai@meta.com>
2024-11-19 14:23:45 -0800

    More logs for QE eval
    Summary: Something is really wrong with QE evals, I wanna figure out what
    
    Differential Revision: D66194255
    
    fbshipit-source-id: 544285b56a30fa0bd5db9a979deb6d6c882c0306

--- td_util/src/qe.rs
+++ td_util/src/qe.rs
@@ -7,6 +7,10 @@
  * of this source tree.
  */
 
+use serde::Serialize;
+
+#[derive(Serialize)]
+#[serde(untagged)]
 pub enum QEParamValue {
     Bool(bool),
     String(String),
@@ -18,24 +22,37 @@ pub fn evaluate_qe(unit_id: u64, universe: &str, param: &str, expect: QEParamVal
     use sandcastle_qe2_client::QE2;
     use tracing::info;
 
+    let value_for_logging: serde_json::Value;
     let qe = crate::executor::run_as_sync(QE2::from_unit_id(unit_id, &[universe]));
-    let ret = match expect {
+    let ret = match &expect {
         QEParamValue::Bool(expect) => {
             let qe_value = qe.get_bool(universe, param, false);
-            qe_value == expect
+            value_for_logging = qe_value.into();
+            qe_value == *expect
         }
         QEParamValue::String(expect) => {
             let qe_value = qe.get_string(universe, param, "");
-            qe_value == expect
+            value_for_logging = qe_value.clone().into();
+            qe_value == *expect
         }
         QEParamValue::Int(expect) => {
             let qe_value = qe.get_int(universe, param, 0);
-            qe_value == expect
+            value_for_logging = qe_value.into();
+            qe_value == *expect
         }
     };
 
-    info!("Check {param} from QE {universe}: {ret}");
-    crate::scuba!(event: VERSE_QE_CHECK, data: json!({ "param": param, "universe": universe, "result": ret }));
+    let expect_str = serde_json::to_string(&expect).unwrap_or_else(|_| "<unknown>".into());
+    info!(
+        "Check {param} from QE {universe}, value {value_for_logging} (expected {expect_str}): {ret}"
+    );
+    crate::scuba!(event: VERSE_QE_CHECK, data: json!({
+        "param": param,
+        "universe": universe,
+        "value": value_for_logging,
+        "expect": expect,
+        "result": ret,
+    }));
     ret
 }
 
