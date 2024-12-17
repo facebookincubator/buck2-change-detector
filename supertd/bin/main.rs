de5fa4dca1a59f3d12c0fa58b873a5cce91d5c02
Aniket Mathur <aniketm@meta.com>
2024-12-17 12:32:21 -0800

    td/supertd: Make supertd::main async
    Summary: So we can run ranker async. Avoids nasty nested `run_as_sync` surprises
    
    Reviewed By: rjbailey
    
    Differential Revision: D67340679
    
    fbshipit-source-id: 1560a720ab5a55e77d297108a9cb3086bf959d8b

--- supertd/bin/main.rs
+++ supertd/bin/main.rs
@@ -17,7 +17,6 @@ use clap::FromArgMatches;
 use clap::Parser;
 use fbinit::FacebookInit;
 use td_util::cli::get_args;
-use td_util::executor::run_as_sync;
 
 /// Generic binary for the pieces of the new target-determinator framework.
 #[allow(clippy::large_enum_variant)] // Only one instance, so not a big deal
@@ -42,8 +41,7 @@ enum Args {
 }
 
 #[fbinit::main]
-
-pub fn main(fb: FacebookInit) -> ExitCode {
+pub async fn main(fb: FacebookInit) -> ExitCode {
     let _guard = td_util::init(fb);
 
     let mut command = Args::command();
@@ -78,7 +76,7 @@ pub fn main(fb: FacebookInit) -> ExitCode {
         #[cfg(fbcode_build)]
         Args::VerifiableMatcher(args) => verifiable_matcher::main(args),
         #[cfg(fbcode_build)]
-        Args::Ranker(args) => run_as_sync(ranker::main(args)),
+        Args::Ranker(args) => ranker::main(args).await,
         #[cfg(fbcode_build)]
         Args::Rerun(args) => rerun::main(fb, args),
         #[cfg(fbcode_build)]
