ab8e7bb65d427b65d03f837d91b53b41893b48e7
generatedunixname89002005232357 <generatedunixname89002005232357@meta.com>
2026-03-31 13:30:14 -0700

    Revert D98796895
    Summary:
    This diff reverts D98796895
    https://www.internalfb.com/diff/D98796895?dst_version_fbid=1162436245906318&transaction_fbid=918835680997056
    
    Depends on D98796895
    
    Differential Revision: D98962704
    
    fbshipit-source-id: a8220b15dbdf8fcc007b2f5158be546dc62a6999

--- td_util/src/project.rs
+++ td_util/src/project.rs
@@ -48,7 +48,6 @@ pub enum TdProject {
     Fbandroid,
     Fbobjc,
     Genai,
-    Infrasilicon,
     Mobile,
     RL,
     Wacommon,
@@ -92,7 +91,6 @@ mod tests {
         assert!(TdProject::Fbandroid.is_fbsource());
         assert!(TdProject::Fbobjc.is_fbsource());
         assert!(TdProject::Genai.is_fbsource());
-        assert!(TdProject::Infrasilicon.is_fbsource());
         assert!(TdProject::Mobile.is_fbsource());
         assert!(TdProject::RL.is_fbsource());
         assert!(TdProject::Wacommon.is_fbsource());
